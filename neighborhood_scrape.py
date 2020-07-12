c7a1f1910b44a43eeaa8665791f6840a3a81ab82
lucyrichmond <61097963+lucyrichmond@users.noreply.github.com>
2020-07-12 15:47:59 -0400

    Delete neighborhood_scrape.py

--- neighborhood_scrape.py
+++
@@ -1,8 +0,0 @@
-import requests
-import urllib.request
-import time
-from bs4 import BeautifulSoup
-
-url = 'https://www.chicagomag.com/Chicago-Magazine/Neighborhood-Guides/'
-response = requests.get(url)
-
